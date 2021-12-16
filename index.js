32d83ddf44c7855b0a7e549beb2081dc8b75070b
TheNoa96 <nicolas.aravena96@gmail.com>
2021-12-16 00:08:53 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+let who = ['The dog','My grandma','His turtle','My bird'];
+let action = ['ate','peed','crushed','broke'];
+let what = ['my homework', 'the keys', 'the car'];
+let when = ['before the class','right on time','when I finished','during my lunch','while I was praying'];
+
+let who = Math.floor(Math.random()*4)+0
+
+let action = Math.floor(Math.random()*4)+0
+
+let what = Math.floor(Math.random()*4)+0
+
+let when = Math.floor(Math.random()*4)+0
+
+console.log(who[who],action[action],what[what],when[when])
